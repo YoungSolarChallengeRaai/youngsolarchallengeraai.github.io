b9cf92ed279d4c945d9e6426995ec01cfc7f47af
Bauke <baukewestendorp@gmail.com>
2020-02-25 23:50:46 +0100

    Cleaned up the project a bit.

--- scripts/game.js
+++ scripts/game.js
@@ -1,30 +1,28 @@
-const header = document.getElementsByClassName("header")[0];
+const header = document.getElementsByClassName('header')[0];
 
-function setup(){
-    createCanvas(header.offsetWidth,windowHeight-header.offsetHeight-100)
-    document.getElementsByClassName("page-content")[0].append(document.getElementById("defaultCanvas0"))
+function setup() {
+	createCanvas(header.offsetWidth, windowHeight - header.offsetHeight - 100);
+	document.getElementsByClassName('page-content')[0].append(document.getElementById('defaultCanvas0'));
 }
 
-function draw(){
-    background(255)
-    stroke(0);
-    strokeWeight(5)
-    translate(width/2,height/2);
-    point(0,0);
+function draw() {
+	background(255);
+	stroke(0);
+	strokeWeight(5);
+	translate(width / 2, height / 2);
+	point(0, 0);
 
-    dash();
+	dash();
 }
 
 function windowResized() {
-    resizeCanvas(header.offsetWidth,windowHeight-header.offsetHeight-100);
+	resizeCanvas(header.offsetWidth, windowHeight - header.offsetHeight - 100);
 }
 
-function boat(){
+function boat() {}
 
-}
-
-function dash(){
-    fill(120)
-    stroke(1)
-    rect(-width/2,height/2-100,width,100)
+function dash() {
+	fill(120);
+	stroke(1);
+	rect(-width / 2, height / 2 - 100, width, 100);
 }
